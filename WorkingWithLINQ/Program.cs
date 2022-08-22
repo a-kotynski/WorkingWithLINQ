691a89b05f6bb425998e2facdb6cc12bb997fb71
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-08-22 13:02:18 +0200

    before

--- WorkingWithLINQ/Program.cs
+++ WorkingWithLINQ/Program.cs
@@ -13,7 +13,7 @@ namespace WorkingWithLINQ
         public string VIN { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
-        public int Year { get; set; }
+        public int Year { get; set; } 
         public double StickerPrice { get; set; }
     }
     internal class Program
